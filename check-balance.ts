a518e4a90b72c49825c19918de6d9e0c0632cb41
Kaleb-Rupe <57547999+Kaleb-Rupe@users.noreply.github.com>
2024-02-01 03:17:03 -0500

    Reverted backt to devnet wallet SOL token checker with Error handling.

--- check-balance.ts
+++ check-balance.ts
@@ -1,30 +1,38 @@
 import { Connection, LAMPORTS_PER_SOL, PublicKey } from "@solana/web3.js";
-import { getDomainKeySync, NameRegistryState } from "@bonfida/spl-name-service";
+
+//** This will be used for mainnet to lookup .sol domains */
+// import { getDomainKeySync, NameRegistryState } from "@bonfida/spl-name-service";
 
 const suppliedPublicKey = process.argv[2];
 
 const SOLANA_CONNECTION = new Connection(
-  "https://api.mainnet.solana.com",
+  "https://api.devnet.solana.com",
   "confirmed"
 );
 
-let publicKey;
+//** This is for when you want to store the public key globally */
+
+// let publicKey;
 
 try {
   // Check if a public key is provided
   if (!suppliedPublicKey) {
-    throw new Error("Provide a public key to check the balance of!");
+    throw new Error("Provide a public key to check the balance!");
   }
 
   // Check if the provided key is of the correct length
-  if (suppliedPublicKey.length === 44) {
-    publicKey = new PublicKey(suppliedPublicKey);
-  } else {
-    // Assume it's a custom representation and convert it
-    // You need to define your own logic for this conversion
-    publicKey = getPublicKeyFromSolDomain();
+  if (suppliedPublicKey.length !== 44) {
+    throw new Error(`${suppliedPublicKey} is not a valid address!`);
   }
 
+  //**Noted out for now when in devnet as this feature must be connected to mainnet */
+  //   else {
+  //     // Assume it's a custom representation and convert it
+  //     // You need to define your own logic for this conversion
+  //     publicKey = getPublicKeyFromSolDomain();
+  //   }
+
+  const publicKey = new PublicKey(suppliedPublicKey);
   try {
     const balanceInLamports = await SOLANA_CONNECTION.getBalance(publicKey);
     const balanceInSOL = balanceInLamports / LAMPORTS_PER_SOL;
@@ -39,11 +47,13 @@ try {
   console.error("Error", err.message);
 }
 
-async function getPublicKeyFromSolDomain() {
-  const { pubkey } = getDomainKeySync(suppliedPublicKey);
-  const owner = (
-    await NameRegistryState.retrieve(SOLANA_CONNECTION, pubkey)
-  ).registry.owner.toBase58();
-  console.log(`The owner of SNS Domain: ${suppliedPublicKey} is: `, owner);
-  return owner;
-}
+//**This is here for when connected to mainnet and you want to look up somes .sol domain and find out how much SOL they are holding. */
+
+// async function getPublicKeyFromSolDomain() {
+//   const { pubkey } = getDomainKeySync(suppliedPublicKey);
+//   const owner = (
+//     await NameRegistryState.retrieve(SOLANA_CONNECTION, pubkey)
+//   ).registry.owner.toBase58();
+//   console.log(`The owner of SNS Domain: ${suppliedPublicKey} is: `, owner);
+//   return owner;
+// }
